11087f92d78feb2945fc1deb0dbf9b9a61a9100f
Le Duy Anh <lenduya@gmail.com>
2023-06-14 17:21:55 +0200

    feat (output): add altterms and games fields

--- discord_util.py
+++ discord_util.py
@@ -1,4 +1,5 @@
 import discord
+import utils
 
 
 def create_embed(title, item, color, author, avatar):
@@ -9,7 +10,13 @@ def create_embed(title, item, color, author, avatar):
     )
     embed.set_author(name=author, icon_url=avatar)
 
-    embed.add_field(name="Games", value=item["games"], inline=True)
+    if item["games"]:
+        embed.add_field(name="Games", value=item["games"], inline=True)
+
+    if item["altterm"]:
+        embed.add_field(name="Synonyms", value=item["altterm"], inline=True)
+
+    embed.add_field(name="Source", value=utils.create_source(searched_term=title), inline=False)
 
     return embed
 
