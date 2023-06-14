c6234076b20cc7f43af539965e653132eeac676b
Le Duy Anh <lenduya@gmail.com>
2023-06-14 17:12:42 +0200

    feat (output): add the Games field

--- discord_util.py
+++ discord_util.py
@@ -8,6 +8,9 @@ def create_embed(title, item, color, author, avatar):
         color=color
     )
     embed.set_author(name=author, icon_url=avatar)
+
+    embed.add_field(name="Games", value=item["games"], inline=True)
+
     return embed
 
 
