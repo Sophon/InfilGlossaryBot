e2da6da537d29ec15e6f05437b5e68af3b8d57fc
Le Duy Anh <lenduya@gmail.com>
2023-06-14 17:28:14 +0200

    feat (output): add the Gif field

--- discord_util.py
+++ discord_util.py
@@ -1,4 +1,5 @@
 import discord
+import utils
 
 
 def create_embed(title, item, color, author, avatar):
@@ -9,7 +10,17 @@ def create_embed(title, item, color, author, avatar):
     )
     embed.set_author(name=author, icon_url=avatar)
 
-    embed.add_field(name="Games", value=item["games"], inline=True)
+    if 'games' in item:
+        embed.add_field(name="Games", value=item["games"], inline=True)
+
+    if 'altterm' in item:
+        embed.add_field(name="Synonyms", value=item["altterm"], inline=True)
+
+    if 'video' in item:
+        link = utils.create_gfycat_link(item["video"][0])
+        embed.add_field(name="Gif", value=utils.wrap_link(link), inline=False)
+
+    embed.add_field(name="Source", value=utils.create_source(searched_term=title), inline=False)
 
     return embed
 
